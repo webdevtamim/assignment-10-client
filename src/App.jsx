ccd10d73894412b717f47b4818bd624888f0514e
webdevtamim <tamimahmed12.bd@gmail.com>
2023-10-19 11:44:55 +0600

    set routes and add error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>BRAND SHOP</h1>
+      <Outlet></Outlet>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <h2>Home</h2>
+      },
+      {
+        path: '/addProduct',
+        element: <h2>Add Product</h2>
+      },
+      {
+        path: '/myCart',
+        element: <h2>My Cart</h2>
+      },
+      {
+        path: '/register',
+        element: <h2>Register</h2>
+      },
+      {
+        path: '/login',
+        element: <h2>Login</h2>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
