46984434666047f64021bfe9c3ec6ab2e34ae150
webdevtamim <tamimahmed12.bd@gmail.com>
2023-10-21 17:33:27 +0600

    delicious food banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,15 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <CateSection></CateSection>
+            <div className="bg-food-section bg-no-repeat bg-cover mt-28">
+                <div className="max-w-[1000px] mx-auto py-28 space-y-5 px-5">
+                    <p className='font-lobster text-2xl text-[#eb0029]'>THE BEST FOOD SHOP</p>
+                    <div>
+                        <h2 id='special-text' className='font-bold  lg:text-7xl text-5xl text-transparent'>DELICIOUS</h2>
+                        <h2 id='special-text' className='font-bold lg:text-7xl text-5xl text-transparent'>FOOD</h2>
+                    </div>
+                </div>
+            </div>
             <HomeProduct></HomeProduct>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       backgroundImage: {
         'head-bg': "url('assets/header-bg.jpg')",
         'foot-bg': "url('assets/footer-bg.png')",
+        'food-section': "url('assets/foodSection.jpg')",
       },
       fontFamily: {
         lobster: "'Lobster Two', sans-serif",
